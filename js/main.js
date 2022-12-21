54e16ef90ffe6684dc1556515441b1403100e218
taniamosch <116739504+taniamosch@users.noreply.github.com>
2022-12-21 18:56:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 (function ($) {
     "use strict";
 
@@ -117,3 +119,20 @@
     
 })(jQuery);
 
+function createPopup(id){
+    let popupNode = document.querySelector(id);
+    let overlay = popupNode.querySelector(".overlay");
+    let closeBtn = popupNode.querySelector(".close-btn");
+    function openPopup(){
+        popupNode.classList.add("active");
+    }
+    function closePopup(){
+        popupNode.classList.remove("active");
+
+    }
+    overlay.addEventListener("click", closePopup);
+    closeBtn.addEventListener("click", closePopup);
+    return openPopup;
+}
+let popup = createPopup("#popup");
+document.querySelector("#open-popup").addEventListener("click",popup);
