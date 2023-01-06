ad1fa53c29cdfe14ba479403c9016e25cf107884
dirollo13 <59145791+dirollo13@users.noreply.github.com>
2023-01-06 18:38:05 +0200

    as

--- php/connection.php
+++ php/connection.php
@@ -1,12 +1,32 @@
 <?php
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $dbname = "mysql";
-    
-    if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-    {
-    
-        die("failed to connect!");
+    $host='localhost';
+    $db = 'chess';
+    require_once "db_upass.php";
+
+    $user=$DB_USER;
+    $pass=$DB_PASS;
+
+
+    if(gethostname()=='users.iee.ihu.gr') {
+        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
+    } else {
+            $pass=null;
+            $mysqli = new mysqli($host, $user, $pass, $db);
     }
+
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: (" . 
+        $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    // $dbhost = "localhost";
+    // $dbuser = "root";
+    // $dbpass = "";
+    // $dbname = "mysql";
+    
+    // if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+    // {
+    
+    //     die("failed to connect!");
+    // }
 ?>
