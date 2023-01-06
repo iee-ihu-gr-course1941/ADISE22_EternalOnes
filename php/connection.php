9dcffe14b4de0caa4208dfd73e926434c0c9cb90
Nivog98 <33768602+Nivog98@users.noreply.github.com>
2023-01-06 12:57:03 +0200

    Add files via upload

--- php/connection.php
+++ php/connection.php
@@ -2,7 +2,7 @@
     $dbhost = "localhost";
     $dbuser = "root";
     $dbpass = "";
-    $dbname = "users_db";
+    $dbname = "mysql";
     
     if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
     {
