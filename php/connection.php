be5f47de51b2ec576bbf9a917aabf2488b4daa04
dirollo13 <59145791+dirollo13@users.noreply.github.com>
2023-01-06 18:39:36 +0200

    con

--- php/connection.php
+++ php/connection.php
@@ -8,7 +8,7 @@
 
 
     if(gethostname()=='users.iee.ihu.gr') {
-        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
+        $mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2016/it164644/mysql/run/mysql.sock');
     } else {
             $pass=null;
             $mysqli = new mysqli($host, $user, $pass, $db);
