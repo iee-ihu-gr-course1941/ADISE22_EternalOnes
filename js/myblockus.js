40a3dc7676aa842daf0129334764d78825e719a3
CaptainRexGr <captainrexgreece@gmail.com>
2023-01-07 21:45:17 +0200

    blockus board

---
+++ js/myblockus.js
@@ -0,0 +1,13 @@
+function draw_empty_board() {
+    var t='<table id="chess_table">';
+    for(var i=20;i>0;i--) {
+    t += '<tr>';
+    for(var j=1;j<21;j++) {
+    t += '<td class="chess_square", id="square_'+j+'_'+i+'">' + j +','+i+'</td>';
+    }
+    t+='</tr>';
+    }
+    t+='</table>';
+    $('#blockus_board').html(t);
+}    
+    $(draw_empty_board);
