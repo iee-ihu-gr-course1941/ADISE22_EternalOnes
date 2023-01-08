3f9ff133ba0e0ad0e8820425b30ef7021613d98b
CaptainRexGr <captainrexgreece@gmail.com>
2023-01-08 12:48:48 +0200

    blokus board is up and running

--- js/myblockus.js
+++ js/myblockus.js
@@ -1,6 +1,6 @@
 function draw_empty_board() {
     var t='<table id="chess_table">';
-    for(var i=20;i>0;i--) {
+    for(var i=1;i<21;i++) {
     t += '<tr>';
     for(var j=1;j<21;j++) {
     t += '<td class="chess_square", id="square_'+j+'_'+i+'">' + j +','+i+'</td>';
