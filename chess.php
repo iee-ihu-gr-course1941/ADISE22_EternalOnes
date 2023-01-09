62539496906818cd0ccf43cf555a046bf7dc2732
dirollo13 <59145791+dirollo13@users.noreply.github.com>
2023-01-09 21:56:56 +0200

    status

--- chess.php
+++ chess.php
@@ -2,31 +2,38 @@
 
 require_once "./php/connection.php";
 require_once "./php/board.php";
+require_once "./php/game.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 //$request = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
 // Σε περίπτωση που τρέχουμε php –S
 $input = json_decode(file_get_contents('php://input'),true);
-print $request;
+// print $request;
 
 switch ($r=array_shift($request)) {
 case 'board' :
 switch ($b=array_shift($request)) {
 case '':
 case null: handle_board($method);break;
-case 'piece': //handle_piece($method, $request[0],$request[1],$input);
+case 'piece': handle_piece($method, $request[0],$request[1],$input);
 break;
-case 'player': //handle_player($method, $request[0],$input);
+case 'player': handle_player($method, $request[0],$input);
 break;
 default: header("HTTP/1.1 404 Not Found");
 break;
-}
-break;
-default:
+case 'status': 
+    if(sizeof($request)==0) {handle_status($method);}
+    else {header("HTTP/1.1 404 Not Found");}
+    break;
+case 'players': handle_player($method, $request,$input);
+        break;
+default: 	
 header("HTTP/1.1 404 Not Found");
 exit;
 }
+
+
 function handle_board($method) {
     if($method=='GET'){
         show_board();
@@ -34,6 +41,21 @@ function handle_board($method) {
         reset_board();
     }else {
         header('HTTP/1.1405 Method Not Allowed');
+    }
+}
+
+function handle_piece($method, $x,$y,$input) {
+    ;
+}
+
+function handle_player($method, $p,$input) {
+    ;
+}
+function handle_status($method) {
+    if($method=='GET') {
+        show_status();
+    } else {
+        header('HTTP/1.1 405 Method Not Allowed');
     }
 }
 ?>
