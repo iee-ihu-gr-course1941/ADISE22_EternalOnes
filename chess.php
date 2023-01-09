0a3f40a36d0f00c9bb63762101c9aee2688f5d60
dirollo13 <59145791+dirollo13@users.noreply.github.com>
2023-01-09 18:17:46 +0200

    api4

--- chess.php
+++ chess.php
@@ -1,21 +1,23 @@
 <?php
 
+require_once "./php/connection.php";
+require_once "./php/board.php";
+
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
  //$request = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
 // Σε περίπτωση που τρέχουμε php –S
 $input = json_decode(file_get_contents('php://input'),true);
-print_r($request);
-exit;
+
 
 switch ($r=array_shift($request)) {
 case 'board' :
 switch ($b=array_shift($request)) {
 case '':
 case null: handle_board($method);break;
-case 'piece': handle_piece($method, $request[0],$request[1],$input);
+case 'piece': //handle_piece($method, $request[0],$request[1],$input);
 break;
-case 'player': handle_player($method, $request[0],$input);
+case 'player': //handle_player($method, $request[0],$input);
 break;
 default: header("HTTP/1.1 404 Not Found");
 break;
@@ -24,5 +26,14 @@ break;
 default:
 header("HTTP/1.1 404 Not Found");
 exit;
+}
+function handle_board($method) {
+    if($method=='GET'){
+        show_board();
+    }else if ($method=='POST'){
+       // reset_board();
+    }else {
+        header('HTTP/1.1405 Method Not Allowed');
+    }
 }
 ?>
