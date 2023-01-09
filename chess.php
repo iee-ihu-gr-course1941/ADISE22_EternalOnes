e1a8b6f51297d5ee92eaf8a0eac98c1d4e8d9e95
dirollo13 <59145791+dirollo13@users.noreply.github.com>
2023-01-09 22:07:37 +0200

    api_fix

--- chess.php
+++ chess.php
@@ -1,50 +1,50 @@
 <?php
 
-require_once "./php/connection.php";
+require_once "./php/connection.php"; 
 require_once "./php/board.php";
 require_once "./php/game.php";
 
+
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-//$request = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
-// Σε περίπτωση που τρέχουμε php –S
+// $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
+// Σε περίπτωση που τρέχουμε php –S 
 $input = json_decode(file_get_contents('php://input'),true);
-// print $request;
 
 switch ($r=array_shift($request)) {
-case 'board' :
-switch ($b=array_shift($request)) {
-case '':
-case null: handle_board($method);break;
-case 'piece': handle_piece($method, $request[0],$request[1],$input);
-break;
-case 'player': handle_player($method, $request[0],$input);
-break;
-default: header("HTTP/1.1 404 Not Found");
-break;
-case 'status': 
-    if(sizeof($request)==0) {handle_status($method);}
-    else {header("HTTP/1.1 404 Not Found");}
-    break;
-case 'players': handle_player($method, $request,$input);
-        break;
-default: 	
-header("HTTP/1.1 404 Not Found");
-exit;
+    case 'board' : 
+	switch ($b=array_shift($request)) {
+		case '': 
+		case null: handle_board($method);break;
+		case 'piece': handle_piece($method, $request[0],$request[1],$input);
+					break;
+		case 'player': handle_player($method, $request[0],$input);
+					break;
+		default: header("HTTP/1.1 404 Not Found");
+				break;
+	}
+		break;
+	case 'status': 
+		if(sizeof($request)==0) {handle_status($method);}
+		else {header("HTTP/1.1 404 Not Found");}
+		break;
+	case 'players': handle_player($method, $request,$input);
+			break;
+    default: 	
+	header("HTTP/1.1 404 Not Found");
+	exit;
 }
-}
-
 
 function handle_board($method) {
-    if($method=='GET'){
-        show_board();
-    }else if ($method=='POST'){
-        reset_board();
-    }else {
-        header('HTTP/1.1405 Method Not Allowed');
+    if($method=='GET') {
+            show_board();
+    } else if ($method=='POST') {
+           reset_board();
+    } else {
+        header('HTTP/1.1 405 Method Not Allowed');
     }
+    
 }
-
 function handle_piece($method, $x,$y,$input) {
     ;
 }
@@ -52,6 +52,7 @@ function handle_piece($method, $x,$y,$input) {
 function handle_player($method, $p,$input) {
     ;
 }
+
 function handle_status($method) {
     if($method=='GET') {
         show_status();
