cc3ff8d3c20f45c94bc38a39de21f28182c2a0a6
CaptainRexGr <captainrexgreece@gmail.com>
2023-01-09 17:40:27 +0200

    css vol 1

--- js/myblockus.js
+++ js/myblockus.js
@@ -1,13 +1,13 @@
 function draw_empty_board() {
-    var t='<table id="chess_table">';
+    var t='<table id="blokus_table">';
     for(var i=1;i<21;i++) {
     t += '<tr>';
     for(var j=1;j<21;j++) {
-    t += '<td class="chess_square", id="square_'+j+'_'+i+'">' + j +','+i+'</td>';
+    t += '<td class="blokus_square", id="square_'+j+'_'+i+'">' + j +','+i+'</td>';
     }
     t+='</tr>';
     }
     t+='</table>';
-    $('#blockus_board').html(t);
+    $('#blokus_board').html(t);
 }    
     $(draw_empty_board);
