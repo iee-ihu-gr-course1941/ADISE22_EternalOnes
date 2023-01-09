2d4b06159cd3926007eb08714e9ac2a218943192
dirollo13 <59145791+dirollo13@users.noreply.github.com>
2023-01-09 19:59:39 +0200

    cm

--- chess.php
+++ chess.php
@@ -5,7 +5,7 @@ require_once "./php/board.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
- $request = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+// $request = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
 // Σε περίπτωση που τρέχουμε php –S
 $input = json_decode(file_get_contents('php://input'),true);
 
